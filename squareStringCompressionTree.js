66dad148e6e717a939a46f8af8c3bf8089c56ee8
Oluwatoyin Amushan <88109099+oluwatoyinca@users.noreply.github.com>
2023-10-14 14:33:46 -0600

    Update squareStringCompressionTree.js, switch to class

--- squareStringCompressionTree.js
+++ squareStringCompressionTree.js
@@ -14,45 +14,53 @@ ii. Build a tree with nodes of each unique character, like so:
 */
 
 
-const node = (char, count, leftNode, rightNode) => {
-  return {
-    char,
-    count: leftNode != null ? leftNode.count + rightNode.count :count,
-    leftNode,
-    rightNode
+class Node {
+  constructor(char, count, leftNode, rightNode){
+    this.char = char
+    this.count = leftNode != null ? leftNode.count + rightNode.count : count
+    this.leftNode = leftNode
+    this.rightNode = rightNode
   }
 }
 
-const uniqueCharCount = (str) => {
-  const result = {}
-  for(let x of str){
-    if(result.hasOwnProperty(x)) result[x]+=1
-    else result[x] = 1
+class CompressionTree {
+  constructor(str) {
+    this.str = str
+    this.tree = {}
+    this.buildTree()
   }
-  return result
+  
+  uniqueCharCount() {
+    const result = {}
+    for(let x of this.str){
+      if(result.hasOwnProperty(x)) result[x]+=1
+      else result[x] = 1
+    }
+    return result
+  }
+  
+  buildTree() {
+    const charCountMap = this.uniqueCharCount()
+    const sortedCharCountMap = Object.keys(charCountMap).sort((a,b) => charCountMap[a] - charCountMap[b])
+    const nodeStack = []
+  
+    for(let i = sortedCharCountMap.length - 1; i>=0; i--) {
+      nodeStack.push(new Node(sortedCharCountMap[i], charCountMap[sortedCharCountMap[i]], null, null))
+    }
+  
+    let parentNode = nodeStack.pop()
+  
+    while (nodeStack.length > 0) {
+      let node1
+      let node2
+        node1 = parentNode
+        node2 = nodeStack.pop()
+        parentNode = new Node('#', null, node2, node1)
+    }
+  
+    this.tree = parentNode
+  }
 }
 
-const createTree = (str) => {
-  const charCountMap = uniqueCharCount(str)
-  const sortedCharCountMap = Object.keys(charCountMap).sort((a,b) => charCountMap[a] - charCountMap[b])
-  const nodeStack = []
-  
-  for(let i = sortedCharCountMap.length - 1; i>=0; i--) {
-    nodeStack.push(node(sortedCharCountMap[i], charCountMap[sortedCharCountMap[i]], null, null))
-  }
-  
-  let parentNode = nodeStack.pop()
-  
-  while (nodeStack.length > 0) {
-    let node1
-    let node2
-      node1 = parentNode
-      node2 = nodeStack.pop()
-      parentNode = node('#', null, node2, node1)
-  }
-  
-  return parentNode
-}
-
-console.log(createTree("aaabbccccd"))
-
+const trees = new CompressionTree('abcddcaaca')
+console.log(trees.tree)
