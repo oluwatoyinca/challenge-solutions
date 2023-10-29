e0e80f8ce2d6f55f9af00a05ca079d85936186aa
Oluwatoyin Amushan <88109099+oluwatoyinca@users.noreply.github.com>
2023-10-28 23:24:38 -0600

    Update squareSocksMatch.js added question

--- squareSocksMatch.js
+++ squareSocksMatch.js
@@ -1,3 +1,23 @@
+/*
+Square Techiical Screen.
+You are given information on a Sock object such as color and foot (left or right). For example, consider the below as input:
+black and left
+blue and right
+pink and right
+pink and left
+black and right
+black and right
+blue and left
+
+You have to write a method which takes the following input and return the list of Sock object pairs (same color, different foot) which are:
+
+[0, 4] OR [0, 5]
+[1, 6]
+[2, 3]
+
+Note if a sock is repeated in a pair then only return 1 pair where that sock is used.
+*/
+
 class SocksMatch{
   constructor() {
     this.socksStore = {}
@@ -15,9 +35,7 @@ class SocksMatch{
         matchedSocks.push([this.socksStore[sockPair], i])
         delete this.socksStore[sockPair]
       }
-      else {
-        this.socksStore[socks[i]] = i
-      }
+      else this.socksStore[socks[i]] = i
     }
     return matchedSocks
   }
@@ -25,4 +43,4 @@ class SocksMatch{
 
 const socks = new SocksMatch()
 // returns [[2, 3], [0, 4], [1, 6]]
-console.log(socks.matchSocks(['black and left', 'blue and left', 'pink and right', 'pink and left', 'black and right', 'black and right', 'blue and right']))
+console.log(socks.matchSocks(['black and left', 'blue and right', 'pink and right', 'pink and left', 'black and right', 'black and right', 'blue and left']))
